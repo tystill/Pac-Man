2c7e01c7e7b4e83d18ef1e6fbbe9be66b03f3008
Ty Still <trstill@ucsb.edu>
2020-10-16 21:06:09 -0700

    Map and Movement
    Setting map and movement

---
+++ Unity Pac-Man/Assets/Scripts/PacMan.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PacMan : MonoBehaviour
+{
+
+    public float speed = 4f;
+    private Vector2 direction = Vector2.zero;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckInput();
+        Move();
+    }
+
+    void CheckInput()
+    {
+        if (Input.GetKeyDown("j"))
+        {
+            direction = Vector2.left;
+
+        }
+        else if (Input.GetKeyDown("k"))
+        {
+            direction = Vector2.right;
+
+        }
+        else if (Input.GetKeyDown("i"))
+        {
+            direction = Vector2.up;
+
+        }
+        else if (Input.GetKeyDown("m"))
+        {
+            direction = Vector2.down;
+
+        }
+    }
+
+    void Move()
+    {
+        transform.localPosition += (Vector3)(direction * speed) * Time.deltaTime;
+    }
+
+    void UpdateOrientation()
+    {
+
+        GetComponent<Animator>().SetFloat("DirX", direction.x);
+        GetComponent<Animator>().SetFloat("DirY", direction.y);
+    }
+}
